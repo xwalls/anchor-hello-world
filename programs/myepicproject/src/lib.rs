c5c4bdc1d436fc500a8dfe8b1d22e98a857966bf
Xavier Paredes <jxavierpp1@gmail.com>
2021-11-06 15:25:48 -0700

    ⚡️✨ Store gif list

--- programs/myepicproject/src/lib.rs
+++ programs/myepicproject/src/lib.rs
@@ -4,11 +4,57 @@ declare_id!("Fg6PaFpoGXkYsidMpWTK6W2BeZ7FEfcYkg476zPFsLnS");
 
 #[program]
 pub mod myepicproject {
-  use super::*;
-  pub fn start_stuff_off(_ctx: Context<StartStuffOff>) -> ProgramResult {
-    Ok(())
-  }
+    use super::*;
+    pub fn start_stuff_off(ctx: Context<StartStuffOff>) -> ProgramResult {
+        // Get a reference to the account.
+        let base_account = &mut ctx.accounts.base_account;
+        // Initialize total_gifs.
+        base_account.total_gifs = 0;
+        Ok(())
+    }
+    // Another function woo!
+    pub fn add_gif(ctx: Context<AddGif>, gif_link: String) -> ProgramResult {
+        // Get a reference to the account and increment total_gifs.
+        let base_account = &mut ctx.accounts.base_account;
+
+        let item = ItemStruct {
+            gif_link: gif_link.to_string(),
+            user_address: *base_account.to_account_info().key,
+        };
+        
+        base_account.gifList.push(item);
+        base_account.total_gifs += 1;
+        Ok(())
+    }
 }
 
+// Attach certain variables to the StartStuffOff context.
 #[derive(Accounts)]
-pub struct StartStuffOff {}
+pub struct StartStuffOff<'info> {
+    #[account(init, payer = user, space = 9000)]
+    pub base_account: Account<'info, BaseAccount>,
+    #[account(mut)]
+    pub user: Signer<'info>,
+    pub system_program: Program <'info, System>,
+}
+
+// Specify what data you want in the AddGif Context.
+// Getting a handle on the flow of things :)?
+#[derive(Accounts)]
+pub struct AddGif<'info> {
+  #[account(mut)]
+  pub base_account: Account<'info, BaseAccount>,
+}
+
+#[derive(Debug, Clone, AnchorSerialize, AnchorDeserialize)]
+pub struct ItemStruct {
+    pub gif_link: String,
+    pub user_address: Pubkey,
+}
+
+// Tell Solana what we want to store on this account.
+#[account]
+pub struct BaseAccount {
+    pub total_gifs: u64,
+    pub gifList: Vec<ItemStruct>,
+}
