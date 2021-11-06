db757fdda7fc446cf47072ad44b9ca183d802605
Xavier Paredes <jxavierpp1@gmail.com>
2021-11-06 15:26:20 -0700

    ✅ Pass solana program test

--- tests/myepicproject.js
+++ tests/myepicproject.js
@@ -1,13 +1,45 @@
 const anchor = require('@project-serum/anchor');
 
+// Need the system program, will talk about this soon.
+const { SystemProgram } = anchor.web3;
+
 const main = async() => {
   console.log("🚀 Starting test...")
 
-  anchor.setProvider(anchor.Provider.env());
+  const provider = anchor.Provider.env();
+  anchor.setProvider(provider);
+
   const program = anchor.workspace.Myepicproject;
-  const tx = await program.rpc.startStuffOff();
+	
+	// Create an account keypair for our program to use.
+  const baseAccount = anchor.web3.Keypair.generate();
+
+  // Call start_stuff_off, pass it the params it needs!
+  let tx = await program.rpc.startStuffOff({
+    accounts: {
+      baseAccount: baseAccount.publicKey,
+      user: provider.wallet.publicKey,
+      systemProgram: SystemProgram.programId,
+    },
+    signers: [baseAccount],
+  });
 
   console.log("📝 Your transaction signature", tx);
+
+  let account = await program.account.baseAccount.fetch(baseAccount.publicKey);
+  console.log('👀 GIF Count', account.totalGifs.toString());
+
+  // Call add_gif!
+  await program.rpc.addGif("insert_a_giphy_link_here",{
+    accounts: {
+      baseAccount: baseAccount.publicKey,
+    },
+  });
+  
+  account = await program.account.baseAccount.fetch(baseAccount.publicKey);
+  console.log('👀 GIF Count', account.totalGifs.toString())
+
+  console.log('GIF LIST', account.gifList)
 }
 
 const runMain = async () => {
